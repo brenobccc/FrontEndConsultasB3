0dc6d7c664743a87a29000b174a55c6f3cc45f23
brenobccc <brenobritobc@gmail.com>
2022-06-15 21:35:11 -0300

    criando mult input

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,8 +1,9 @@
-import {React, useState, useEffect} from 'react';
+import { React, useState, useEffect } from 'react';
 import styled from "styled-components";
 import DivGrafico from './DivGrafico';
-import GraficoVazio from './GraficoVazio'
-
+import GraficoVazio from './GraficoVazio'   
+// import InputForAny from './InputForAny'
+import Select from 'react-select'
 
 const Div = styled.div`
 padding: 0 16px;
@@ -31,19 +32,38 @@ border-radius: 20px;
 // function preencher(){
 //     console.log("teste")
 // }
+const DivF = styled.div`
+background: orange;
+width: 50%;
+`;
 
-export default function Content(props){
-    const [data_ini,setDataIni] = useState('');
-    const [data_fim,setDataFim] = useState('');
-    const [ativo,setAtivo] = useState('');
-    const [ativoAnt,setAtivoAnt] = useState('');
+
+const options = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' },
+    { value: 'test', label: 'teste1'},
+    { value: 'test2', label: 'teste2'},
+    { value: 'test3', label: 'teste3'},
+    { value: 'test4', label: 'teste4'},
+]
+
+export default function Content(props) {
+    const [data_ini, setDataIni] = useState('');
+    const [data_fim, setDataFim] = useState('');
+    const [ativo, setAtivo] = useState('');
+    const [ativoAnt, setAtivoAnt] = useState('');
 
     const [controll, setControll] = useState(true)
-
-    useEffect((ativo)=>{
+    const [selectedOptions, setSelectedOptions] = useState([]);
+    useEffect((ativo) => {
         console.log("mudou")
     })
-    
+
+    useEffect(() => {
+        console.log("valor atualizado:"+JSON.stringify(selectedOptions));
+    })
+
 
     return (
         <Div>
@@ -51,29 +71,33 @@ export default function Content(props){
             <DivForm>
                 <input type="date" id="data_inicial"></input>
                 <input type="date" id="data_final"></input>
-                <input type="text" id="ativo"></input>
-                <button onClick= {()=> {
-                   setDataIni(document.getElementById("data_inicial").value);
-                   setDataFim(document.getElementById("data_final").value);
-                   
-                   setAtivoAnt(ativo);
-                   setAtivo(document.getElementById("ativo").value);
-                   
-                   
 
-                //    if(controll){
-                //         setControll(true);
-                //    }
+                <DivF>
+                <Select isMulti onChange={(item)=> setSelectedOptions(item)} options={options}/>
+                </DivF>
+                <input type="text" id="ativo"></input>
+                <button onClick={() => {
+                    setDataIni(document.getElementById("data_inicial").value);
+                    setDataFim(document.getElementById("data_final").value);
+
+                    setAtivoAnt(ativo);
+                    setAtivo(document.getElementById("ativo").value);
+
+
+
+                    //    if(controll){
+                    //         setControll(true);
+                    //    }
                 }}>
                     enviar
                 </button>
             </DivForm>
-            {console.log("Data inicial:"+data_ini)}
-            
-            { controll == true ? 
-                ( ativo != "" ?   (<DivGrafico dtIni = {data_ini} dtFim = {data_fim} ativos = {ativo} /> ) : <GraficoVazio/>) : ""}
+            {console.log("Data inicial:" + data_ini)}
+
+            {controll == true ?
+                (ativo != "" ? (<DivGrafico dtIni={data_ini} dtFim={data_fim} ativos={ativo} />) : <GraficoVazio />) : ""}
             {/* { controll == true ? setControll(false) : setControll(false)} */}
-            
+
 
         </Div>
     )
